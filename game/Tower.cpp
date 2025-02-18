cb75a8e50dddcb400eb9aafe8b8b984197ed90cf
Armen Deroian <armendero330@gmail.com>
2025-02-18 10:59:39 -0500

    some logic additions for towers

--- game/Tower.cpp
+++ game/Tower.cpp
@@ -7,6 +7,13 @@
 TowerManager::TowerManager(void)
 {
 	gameLocal.Printf("Tower manager created...\n");
+
+	buildMode = false;
+	buildTower = "tower";
+
+	lastWaveStart = -1;
+	lastWaveEnd = -1;
+	waveDelay = 1 * 60 * 10 ^ 3;
 }
 
 TowerManager::~TowerManager(void)
@@ -20,25 +27,66 @@ void TowerManager::Init(void)
 
 void TowerManager::Update(void)
 {
+	// Update Wave
+	if (!wave && gameLocal.GetTime() > (lastWaveEnd + waveDelay)) {
+		wave = new Wave();
+		wave->Init();
+	}
+
+	// Update Towers
+	for (int i = 0; i < towers.Num(); i++)
+	{
+		towers[i]->Update();
+	}
+}
+
+void TowerManager::AddTower(Tower*& tower)
+{
+	towers.Append(tower);
+}
+
+bool TowerManager::CanTowersShoot(void)
+{
+	if (wave) {
+		return wave->HasStarted() && !wave->HasEnded();
+	}
+
+	return false;
 }
 
 Tower::Tower(idPlayer* owner, idStr tower)
 {
 	this->owner = owner;
 	this->tower = tower;
+	init = false;
+	towerEntity = nullptr;
+
+	gameLocal.towerManager->AddTower(this);
 }
 
 Tower::~Tower(void)
 {
+	gameLocal.Printf("Tower destroyed...\n");
+
+	if (towerEntity)
+		delete towerEntity;
+
+	towerEntity = nullptr;
+	init = false;
 }
 
 void Tower::Init(idVec3 origin)
 {
 	this->origin = origin;
+	SpawnTower();
+
+	init = true;
 }
 
 void Tower::SpawnTower()
 {
+	if (towerEntity) return;
+
 	float yaw;
 	idDict dict;
 
@@ -56,13 +104,42 @@ void Tower::SpawnTower()
 	if (newEnt) {
 		gameLocal.Printf("spawned tower '%s'\n", newEnt->name.c_str());
 	}
+	else {
+		gameLocal.Printf("failed to spawn tower\n");
+		return;
+	}
+
+	towerEntity = newEnt;
+	init = true;
 }
 
 void Tower::Update(void)
 {
+	if (!init || !towerEntity) return;
+
+	if (gameLocal.GetTime() % shootDelay == 0)
+		Shoot();
 }
 
 idVec3 Tower::GetOrigin(void)
 {
 	return origin;
 }
+
+bool Tower::CanShoot(void)
+{
+	return gameLocal.towerManager->CanTowersShoot();
+}
+
+void Tower::Shoot(void)
+{
+	if (!CanShoot()) return;
+	ForceShoot();
+}
+
+void Tower::ForceShoot(void)
+{
+	// TODO: shoot logic
+}
+
+
--- game/Tower.h
+++ game/Tower.h
@@ -1,15 +1,42 @@
 #ifndef __TOWER_H__
 #define __TOWER_H__
 
+class Wave {
+public:
+	Wave(void);
+	~Wave(void);
+	void Init(void);
+	void Update(void);
+	bool HasStarted(void);
+	bool HasEnded(void);
+
+public:
+	int startingMonsters;
+	int monstersLeft;
+
+};
+
 class TowerManager {
+public:
+	bool buildMode;
+	char* buildTower;
+
 public:
 	TowerManager(void);
 	~TowerManager(void);
 
 	void Init(void);
-
 	void Update(void);
+	void AddTower(Tower* &tower);
+	bool CanTowersShoot(void);
 
+private:
+	idList<Tower*> towers;
+	Wave* wave;
+
+	int lastWaveStart;
+	int lastWaveEnd;
+	int waveDelay;
 };
 
 class Tower {
@@ -18,14 +45,25 @@ public:
 	Tower(idPlayer* owner, idStr tower);
 	~Tower(void);
 	void Init(idVec3 origin);
-	void SpawnTower();
 	void Update(void);
 	idVec3 GetOrigin(void);
+
+	bool CanShoot(void);
+	void Shoot(void);
+	void ForceShoot(void); // Shoots without checking if we can actually shoot
 
 private:
 	idPlayer* owner;
 	idStr tower;
 	idVec3 origin;
+	idEntity* towerEntity;
+
+	bool init;
+	
+	int shootDelay;
+
+private:
+	void SpawnTower();
 };
 
 #endif // __TOWER_H__
